5df3558fb519ab67279267e4b2dbe31cb274732b
Austin Burger <aburger91@gmail.com>
2020-12-18 10:42:15 -0600

    adds the classifying newswires model

---
+++ classifying_newswires/model.py
@@ -0,0 +1,81 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.datasets import reuters
+from tensorflow.keras import models
+from tensorflow.keras import layers
+
+(train_data, train_labels), (test_data, test_labels) = reuters.load_data(
+  num_words=10000)
+
+
+def vectorize_sequences(sequences, dimension=10000):
+    results = np.zeros((len(sequences), dimension))
+    for i, sequence in enumerate(sequences):
+        results[i, sequence] = 1
+    return results
+
+
+def to_one_hot(labels, dimension=46):
+    results = np.zeros((len(labels), dimension))
+    for i, label in enumerate(labels):
+        results[i, label] = 1
+    return results
+
+
+x_train = vectorize_sequences(train_data)
+x_test = vectorize_sequences(test_data)
+
+one_hot_train_labels = to_one_hot(train_labels)
+one_hot_test_labels = to_one_hot(test_labels)
+
+model = models.Sequential()
+model.add(layers.Dense(64, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(46, activation='softmax'))
+
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
+              metrics=['acc'])
+
+x_val = x_train[:1000]
+partial_x_train = x_train[1000:]
+
+y_val = one_hot_train_labels[:1000]
+partial_y_train = one_hot_train_labels[1000:]
+
+history = model.fit(partial_x_train,
+                    partial_y_train,
+                    epochs=9,
+                    batch_size=512,
+                    validation_data=(x_val, y_val))
+
+
+loss = history.history['loss']
+acc = history.history['acc']
+val_loss = history.history['val_loss']
+val_acc = history.history['val_acc']
+
+epochs = range(1, len(loss) + 1)
+
+results = model.evaluate(x_test, one_hot_test_labels)
+print(results)
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.savefig('plots/tv_graph_loss_9e.png')
+
+plt.clf()
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.savefig('plots/tv_graph_acc_9e.png')
